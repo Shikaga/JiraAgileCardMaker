3e12abaac644f540ce29e25abf050e334e8e08fb
Jon Paul <shikaga@gmail.com>
2012-11-29 11:11:25 +0000

    The app will automatically request Parent Jiras

--- App.js
+++ App.js
@@ -35,27 +35,50 @@ jira.App.prototype.x = function(jiras)
 jira.App.prototype.requestAllJiras = function(jiras)
 {
     this.expectedCallbacks = 0;
+    this.callbacksReceived = 0;
     for (var i=0; i < jiras.length; i++)
     {
         this.expectedCallbacks++;
         var jira = jiras[i];
-		var jiraUrl = "https://jira.caplin.com/rest/api/latest/issue/" + jira + "?jsonp-callback=getJiraCallback";
-		var scriptElement = document.createElement("script");
-		scriptElement.setAttribute("type", "text/javascript");
-		scriptElement.setAttribute("src", jiraUrl);
-		document.head.appendChild(scriptElement);
+	    this.requestJira(jira);
 	}
 }
 
-jira.App.prototype.matchFixVersion = function(e)
+jira.App.prototype.requestJira = function(jira) {
+    var jiraUrl = "https://jira.caplin.com/rest/api/latest/issue/" + jira + "?jsonp-callback=getJiraCallback";
+	var scriptElement = document.createElement("script");
+	scriptElement.setAttribute("type", "text/javascript");
+	scriptElement.setAttribute("src", jiraUrl);
+	document.head.appendChild(scriptElement);
+}
+
+jira.App.prototype.matchesFixVersion = function(jiraObject)
 {
-	var matchesFixVersion = false;
-	var fixVersions = e.fields.fixVersions;
+	var fixVersions = jiraObject.fields.fixVersions;
 	for (var version in fixVersions)
 	{
-		if (fixVersions[version].id == this.fixVersion) matchesFixVersion = true;
+		if (fixVersions[version].id == this.fixVersion) return true;
 	}
-	return matchesFixVersion;
+	return false;
+}
+
+jira.App.prototype.isParentLoaded = function(jiraObject) {
+    
+}
+
+jira.App.prototype.parentsNotLoaded = function() {
+    var parentsNotLoaded = [];
+    for (index in this.jiraMap) {
+        var jira = this.jiraMap[index];
+        var parentId = jira.fields.parent;
+        if (parentId) {
+            var parentKey = jira.fields.parent.key;
+            if (!this.jiraMap[parentKey]) {
+                parentsNotLoaded.push(jira.fields.parent.key);
+            }
+        }
+    }
+    return parentsNotLoaded
 }
 
 jira.App.prototype.getJiraCallback = function(e)
@@ -68,36 +91,41 @@ jira.App.prototype.getJiraCallback = function(e)
 }
 
 jira.App.prototype.renderCards = function() {
+    debugger;
     var cards = 0;
-    console.log(this.jiraMap);
-    for (index in this.jiraMap) {
-        var e = this.jiraMap[index];
-        if (cards%2 == 0) {
-        	var pageElement = document.createElement("div");
-    		pageElement.style.pageBreakAfter = "always";
-    		pageElement.style.clear = "both";
-    		document.getElementById("tickets").appendChild(pageElement);
-    	}
-    	if (this.matchFixVersion(e)) {
-    		this.totalTickets++;
-            var parent = null;
-            var parentSummary = null;
-            if (e.fields.parent) {
-                var parent = e.fields.parent.key;
-                if (this.parentdescription) {   
-                    var parentSummary = this.jiraMap[parent].fields.summary;
+    var parentsNotLoaded = this.parentsNotLoaded();
+    if (parentsNotLoaded.length == 0) {
+        for (index in this.jiraMap) {
+            var jira = this.jiraMap[index];
+            if (cards%2 == 0) {
+            	var pageElement = document.createElement("div");
+        		pageElement.style.pageBreakAfter = "always";
+        		pageElement.style.clear = "both";
+        		document.getElementById("tickets").appendChild(pageElement);
+        	}
+        	if (this.matchesFixVersion(jira)) {
+        		this.totalTickets++;
+                var parent = null;
+                var parentSummary = null;
+                if (jira.fields.parent) {
+                    var parentKey = jira.fields.parent.key;
+                    if (this.parentdescription) {   
+                        var parentSummary = this.jiraMap[parentKey].fields.summary;
+                    }
+                    console.log(parentSummary);
                 }
-                console.log(parentSummary);
-            }
-            
-    		var jiraId = e.key;
-    		var jiraEstimate = e.fields["customfield_10243"];
-    		var jiraSummary = e.fields.summary;
-    		var color = this.colorEnabled ? this.issueTypeColors[e.fields.issuetype.name] : null;
-    		this.addTicket(this.divId,"jira.caplin.com/browse/" + jiraId, jiraId, jiraEstimate, jiraSummary, parent, parentSummary, color, pageElement, this.qrcodeEnabled);
-    	}
-        cards++;
-    };
+                
+        		var jiraId = jira.key;
+        		var jiraEstimate = jira.fields["customfield_10243"];
+        		var jiraSummary = jira.fields.summary;
+        		var color = this.colorEnabled ? this.issueTypeColors[jira.fields.issuetype.name] : null;
+        		this.addTicket(this.divId,"jira.caplin.com/browse/" + jiraId, jiraId, jiraEstimate, jiraSummary, parent, parentSummary, color, pageElement, this.qrcodeEnabled);
+        	}
+            cards++;
+        };
+    } else {
+        this.requestAllJiras(parentsNotLoaded);
+    }
 }
 
 jira.App.prototype.addTicket = function(divId, url, title, estimate, summary, parent, parentSummary, color, pageElement, qrcodeEnabled)
