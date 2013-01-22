7d03eb10f20630e13b4cf54690ac66e3f20fc1f1
Jon Paul <shikaga@gmail.com>
2013-01-22 08:43:40 +0000

    BUG: The JiraNavigator didn't listen to the jira location

--- src/Main.js
+++ src/Main.js
@@ -247,7 +247,11 @@ key('esc', function(){ showInterface(); });
 //{"contents":{"issueKeys":["PCINTS-100","PCX1AQA4-112","PCX1AQA4-120","PCX1AQA4-139","PCX1AQA4-145","PCX1AQA4-149","PSL-68","PSL-109","PSL-161","PSL-163","PSL-167","PSL-174","PSL-179","PSL-181","PSL-182","PSL-183","PSL-184","PSL-185","PSL-186","PSL-187","PSL-188","PSL-189","PSL-190","PSL-191","PSL-192","PSL-194","PSL-195","PSL-196","PSL-197","PSL-198","PSL-199","PSL-200","PSL-203","PSL-208","PSL-209","PSL-211","PSL-213"]}}
 
 //
-
 var jiraNavigatorDiv = document.getElementById("jiraNavigator");
-var jn = new JiraNavigator("https://jira.caplin.com");
-jn.renderInElement(jiraNavigatorDiv);
+function updateJiraNavigator() {
+	var location = document.getElementById("jiraLocation").value;
+	var jn = new JiraNavigator(location);
+	jn.renderInElement(jiraNavigatorDiv);
+}
+
+updateJiraNavigator();
