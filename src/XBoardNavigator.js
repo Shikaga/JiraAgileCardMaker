46f507a2230992835b1629ff27ebca77cdcaf3b7
Jon Paul <shikaga@gmail.com>
2013-09-18 08:22:56 +0100

    Well, I guess I better change my password

---
+++ src/XBoardNavigator.js
@@ -0,0 +1,56 @@
+var RapidBoardNavigator = function(jiraUrl, jiraNavigator) {
+	this.jiraNavigator = jiraNavigator;
+	this.jah = new JiraApiHandler(jiraUrl, this);
+	this.rapidBoardsDropDown = {
+		value: ko.observable(),
+		visible: ko.observable(false),
+		options: ko.observableArray(),
+		change: function() {
+			if (this.rapidBoardsDropDown.value().value !== "none") {
+				this.jah.requestRapidSprints(this.rapidBoardsDropDown.value().value);
+			}
+		}
+	}
+	this.rapidBoardSprintsDropDown = {
+		value: ko.observable(),
+		visible: ko.observable(false),
+		options: ko.observableArray(),
+		change: function() {
+			if (this.rapidBoardSprintsDropDown.value().value !== "none") {
+				this.jah.getRapidBoardSprint(this.rapidBoardsDropDown.value().value, this.rapidBoardSprintsDropDown.value().value);
+			}
+		}
+	}
+}
+
+RapidBoardNavigator.prototype.getDisplayName = function() {
+	return "Rapid Board";
+}
+
+RapidBoardNavigator.prototype.requestTopLevelData = function() {
+	this.jah.requestRapidViews();
+}
+
+RapidBoardNavigator.prototype.init = function() {
+	ko.applyBindingsToNode(document.getElementById("rapidBoardsDropDown"), null, this);
+	ko.applyBindingsToNode(document.getElementById("rapidBoardSprintsDropDown"), null, this);
+}
+
+RapidBoardNavigator.prototype.receiveRapidBoardViews = function(views) {
+	this.rapidBoardsDropDown.visible(true);
+	JiraNavigator.setDropDown(this.rapidBoardsDropDown.options, views.views, "id", "name");
+}
+
+RapidBoardNavigator.prototype.receiveOpenRapidBoardSprints = function(ids) {
+	this.rapidBoardSprintsDropDown.visible(true);
+	JiraNavigator.setDropDown(this.rapidBoardSprintsDropDown.options, ids, "id", "name")
+}
+
+RapidBoardNavigator.prototype.hideAll = function() {
+	this.rapidBoardsDropDown.visible(false);
+	this.rapidBoardSprintsDropDown.visible(false);
+}
+
+RapidBoardNavigator.prototype.receiveJiraCallback = function(jiras) {
+	this.jiraNavigator.receiveJiraCallback(jiras);
+}
